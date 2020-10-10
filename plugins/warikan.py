e3b5f0ad100647940023037b3996b1955708f92b
Yuki Y <yukidmy@gmail.com>
2020-10-11 01:53:50 +0900

    Implement warikanbot with slackbot library

--- plugins/warikan.py
+++ plugins/warikan.py
@@ -1,34 +1,97 @@
 # coding: utf-8
 
-from slackbot.bot import respond_to
+import json
+import psycopg2
+from psycopg2.extras import DictCursor
+
+from slackbot import settings
 from slackbot.bot import listen_to
+from slackbot.bot import respond_to
 
-error_message = 'よくわかんなかった！'
 
-
-@respond_to('リセット')  # メンションにお返事
-def reset_func(message):
-    if reset():
+@respond_to(r'^(?:りせっと|リセット|reset|Reset|RESET)$')
+def reset(message):
+    if reset_func():
         message.reply('おっけ〜リセットしたよ')
     else:
-        message.reply(error_message)
+        message.reply('リセット失敗！　また今度試してね')
 
 
-@respond_to(r'^(?:割り勘|わりかん|warikan) (\d*)')  # メンションにお返事
-def mention_func(message, price):
-    warikan(price)
-    message.reply(price)
+@respond_to(r'^([-+]?\d*)[ 　]')
+@listen_to(r'^([-+]?\d*)[ 　]')
+def warikan(message, price):
+    if message.body['channel'] != settings.CHANNEL_ID:
+        return
+    if message.user['name'] not in [settings.USER1, settings.USER2]:
+        return {'error': '誰だかわかんなかった！！　つらい〜！！'}
+    result = warikan_func(message.user['name'], int(price))
+    if 'error' in result:
+        message.reply(result['error'])
+    elif result['price'] == 0:
+        message.reply('記録したよ！　ぴったり割り勘できたね！')
+    else:
+        formatted_price = '{:,}'.format(result["price"])
+        attachments = [{
+            'fallback': f'{result["from"]} → {result["to"]}: {result["price"]}',
+            'color': 'good',
+            'title': f'{result["from"]} → {result["to"]}',
+            'text': f'{formatted_price} 円'
+        }]
+        message.send_webapi('', json.dumps(attachments))
 
 
-@listen_to(r'^(?:割り勘|わりかん|warikan) (\d*)')  # チャンネル内発言にお返事
-def listen_func(message, price):
-    warikan(price)
-    message.reply(price)
+def reset_func():
+    print('Reset')
+    try:
+        with psycopg2.connect(settings.DATABASE_URL, sslmode='require') as db:
+            with db.cursor(cursor_factory=DictCursor) as cur:
+                df = {settings.USER1: 0, settings.USER2: 0}
+                print({'new': df})
+                query = """UPDATE warikan SET price = %s where name = %s"""
+                for k, v in df.items():
+                    cur.execute(query, (v, k))
+                db.commit()
+        print('Resetted')
+        return True
+    except (Exception, psycopg2.Error) as error:
+        print("Error in update operation", error)
+        return False
 
 
-def warikan(price):
-    return {'name': price}
-
-
-def reset():
-    return True
+def warikan_func(name, price):
+    print('Warikan', name, price)
+    try:
+        with psycopg2.connect(settings.DATABASE_URL, sslmode='require') as db:
+            with db.cursor(cursor_factory=DictCursor) as cur:
+                cur.execute('SELECT * FROM warikan')
+                data = cur.fetchall()
+                df = {}
+                if not data:
+                    df = {settings.USER1: 0, settings.USER2: 0}
+                else:
+                    for d in data:
+                        df[d[0]] = d[1]
+                print({'prev': df})
+                df[name] += price
+                result = {}
+                if df[settings.USER1] > df[settings.USER2]:
+                    df[settings.USER1] -= df[settings.USER2]
+                    df[settings.USER2] = 0
+                    diff = df[settings.USER1] // 2
+                    result = {'from': settings.USERNAME2,
+                              'to': settings.USERNAME1, 'price': diff}
+                else:
+                    df[settings.USER2] -= df[settings.USER1]
+                    df[settings.USER1] = 0
+                    diff = df[settings.USER2] // 2
+                    result = {'from': settings.USERNAME1,
+                              'to': settings.USERNAME2, 'price': diff}
+                print({'new': df})
+                query = """UPDATE warikan SET price = %s where name = %s"""
+                for k, v in df.items():
+                    cur.execute(query, (v, k))
+                db.commit()
+        return result
+    except (Exception, psycopg2.Error) as error:
+        print("Error in update operation", error)
+        return {'error': error}
--- slackbot_settings.py
+++ slackbot_settings.py
@@ -3,14 +3,17 @@
 from dotenv import load_dotenv
 import os
 
-load_dotenv(dotenv_path=".env")  # load local .env file
+load_dotenv(dotenv_path=".env")  # Load local .env file
 
-# For slackbot
+# Slackbot settings
 API_TOKEN = os.environ["TOKEN"]
 DEFAULT_REPLY = "わかんないよ〜ん"
 PLUGINS = ['plugins']
 
-
+# Personal settings
 CHANNEL_ID = os.environ["CHANNEL_ID"]
 USER1 = os.environ["USER1"]
 USER2 = os.environ["USER2"]
+USERNAME1 = os.environ["USERNAME1"]
+USERNAME2 = os.environ["USERNAME2"]
+DATABASE_URL = os.environ["DATABASE_URL"]
